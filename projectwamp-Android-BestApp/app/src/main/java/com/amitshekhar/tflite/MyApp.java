cb57c3787431ae0a8568e1a4a650c29fa5821fbd
Abdoul Aziz Sandotin Coulibaly <abdoulaziz@Abdouls-MacBook-Pro.local>
2019-08-01 15:58:05 -0400

    Android Best App Push

---
+++ projectwamp-Android-BestApp/app/src/main/java/com/amitshekhar/tflite/MyApp.java
@@ -0,0 +1,12 @@
+package com.amitshekhar.tflite;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        SystemClock.sleep(1000);
+    }
+}
